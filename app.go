4b7055cd3f3b219282332cb56b2431d1230efe6f
Levshakov Nikolai <levshakov_nikk@bk.ru>
2022-01-05 11:55:10 +0300

    changed world on Polina

--- app.go
+++ app.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World!")
+		fmt.Fprintf(w, "Hello Polina!")
 	})
 	http.ListenAndServe(":7777", nil)
 }
